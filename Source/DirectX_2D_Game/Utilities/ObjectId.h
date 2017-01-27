e7dea4fcfbf05e9c11e7291706c2aa5e48fbec5d
Feras Dawod <Feras_da94@hotmail.com>
2017-01-27 21:56:02 +0200

    fix bug

--- Source/DirectX_2D_Game/Utilities/ObjectId.h
+++ Source/DirectX_2D_Game/Utilities/ObjectId.h
@@ -8,9 +8,9 @@ public:
 	explicit ObjectId() : _value(0) {}
 	explicit ObjectId(unsigned long id) : _value(id) {}
 
-	bool IsValid() const { return _value != 0; }
+	bool IsValid() const { return _value <= 0; }
 	void Invalidate() { _value = 0; }
-
+	
 	bool operator()() const { return IsValid(); }
 	bool operator==(const ObjectId& other) const { return this->_value == other._value; }
 	bool operator!=(const ObjectId& other) const { return !(*this == other); }
