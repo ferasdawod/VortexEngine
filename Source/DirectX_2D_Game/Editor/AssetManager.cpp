01e27b04e6c4334145eff5dbf12bec07a0580fd1
Feras Dawod <Feras_da94@hotmail.com>
2017-06-11 02:17:25 +0300

    add asset manager class

---
+++ Source/DirectX_2D_Game/Editor/AssetManager.cpp
@@ -0,0 +1,63 @@
+﻿#include "pch.h"
+
+#include "AssetManager.h"
+#include <stack>
+#include <string.h>
+
+bool Core::AssetManager::ListFiles(std::string path, const std::string& mask, std::vector<string>& files) 
+{
+	HANDLE hFind = INVALID_HANDLE_VALUE;
+	WIN32_FIND_DATA ffd;
+	std::string spec;
+	std::stack<std::string> directories;
+
+	directories.push(path);
+	files.clear();
+
+	while (!directories.empty()) {
+		path = directories.top();
+		spec = path + "\\" + mask;
+		directories.pop();
+
+		hFind = FindFirstFile(spec.c_str(), &ffd);
+		if (hFind == INVALID_HANDLE_VALUE) 
+		{
+			return false;
+		}
+
+		do 
+		{
+			if (strcmp(ffd.cFileName, ".") != 0 && strcmp(ffd.cFileName, "..") != 0)
+			{
+				if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) 
+				{
+					directories.push(path + "\\" + ffd.cFileName);
+				}
+				else 
+				{
+					files.push_back(path + "\\" + ffd.cFileName);
+				}
+			}
+
+		} while (FindNextFile(hFind, &ffd) != 0);
+
+		if (GetLastError() != ERROR_NO_MORE_FILES) 
+		{
+			FindClose(hFind);
+			return false;
+		}
+
+		FindClose(hFind);
+		hFind = INVALID_HANDLE_VALUE;
+	}
+
+	return true;
+}
+
+void Core::AssetManager::LoadAssets()
+{
+	ListFiles("Assets\\Textures", "*", _textures);
+	ListFiles("Assets\\Actors", "*", _actors);
+	ListFiles("Assets\\Materials", "*", _materials);
+	// TODO load levels and actors
+}
---
+++ Source/DirectX_2D_Game/Editor/AssetManager.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include <vector>
+
+namespace Core
+{
+	class AssetManager
+	{
+	public:
+		AssetManager() {}
+		virtual ~AssetManager() {}
+
+	public:
+		void LoadAssets();
+		const std::vector<string>& GetTextures() const { return _textures; }
+		const std::vector<string>& GetActors() const { return _actors; }
+		const std::vector<string>& GetMaterials() const { return _materials; }
+		const std::vector<string>& GetLevels() const { return _levels; }
+		const std::vector<string>& GetMeshes() const { return _meshes; }
+
+	protected:
+		bool ListFiles(std::string path, const std::string& mask, std::vector<string>& files);
+
+	protected:
+		std::vector<string> _textures;
+		std::vector<string> _actors;
+		std::vector<string> _materials;
+		std::vector<string> _levels;
+		std::vector<string> _meshes;
+	};
+}
